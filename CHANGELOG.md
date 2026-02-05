905bad26c90ab41398de0bec09bf5e8d5ef6fc95
Juro Oravec <juraj.oravec.josefson@gmail.com>
2026-02-05 20:42:51 +0100

    chore: merge changes from latest djc-core

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,4 +4,4 @@
 
 Initial commit.
 
-This project was forked from [django-components/djc-core](https://github.com/django-components/djc-core) at commit [8682e2c](https://github.com/django-components/djc-core/commit/8682e2c).
+This project was forked from [django-components/djc-core](https://github.com/django-components/djc-core) at commit [49e20dc](https://github.com/django-components/djc-core/commit/49e20dc).
--- packages/py/citry_core/citry_core/html_transform/__init__.py
+++ packages/py/citry_core/citry_core/html_transform/__init__.py
@@ -1,14 +1,8 @@
-import sys
+from typing import TypeAlias
 
 from citry_core import _rust
 
-# TODO - Remove this conditional once Django drops support for Python 3.8 and 3.9
-if sys.version_info >= (3, 10):
-    from typing import TypeAlias
-
-    transform_html: TypeAlias = _rust.html_transform.transform_html
-else:
-    transform_html = _rust.html_transform.transform_html
+transform_html: TypeAlias = _rust.html_transform.transform_html
 
 
 __all__ = ["transform_html"]
